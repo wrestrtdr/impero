11b8fa3461d979929831c22f539924b271d9753e
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-04-30 22:54:53 +0200

    Added impero

--- config/router.php
+++ config/router.php
@@ -7,6 +7,7 @@ return [
             'host' => [
                 'impero.pckg-app',
                 'impero.foobar.si',
+                'impero.gonparty.eu.foobar.si',
             ],
         ],
     ],
