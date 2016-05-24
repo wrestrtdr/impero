beb95b69d6409603565e1710212bc19b8b408ac2
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-05-24 13:58:41 +0200

    Added to composer

--- app/queue/src/Queue.php
+++ app/queue/src/Queue.php
@@ -1,15 +1,7 @@
 <?php
 
-use Impero\Apache\Provider\Config as ApacheProvider;
-use Impero\Ftp\Provider\Config as FtpProvider;
-use Impero\Mysql\Provider\Config as MysqlProvider;
-use Pckg\Auth\Provider\Config as AuthProvider;
-use Pckg\Dynamic\Provider\Config as DynamicProvider;
 use Pckg\Framework\Application;
 use Pckg\Framework\Provider;
-use Pckg\Generic\Middleware\EncapsulateResponse;
-use Pckg\Generic\Provider\Config as GenericProvider;
-use Pckg\Manager\Provider\Config as ManagerProvider;
 use Pckg\Queue\Provider\Queue as QueueProvider;
 
 class Queue extends Provider
@@ -18,18 +10,7 @@ class Queue extends Provider
     public function providers()
     {
         return [
-            ManagerProvider::class,
-            DynamicProvider::class,
-            AuthProvider::class,
-            GenericProvider::class,
             QueueProvider::class,
-        ];
-    }
-
-    public function afterwares()
-    {
-        return [
-            EncapsulateResponse::class,
         ];
     }
 
