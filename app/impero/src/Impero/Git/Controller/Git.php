363091e1e4dc0083cf5a1d37876cc10b3e9cdfec
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-06-08 16:08:50 +0200

    Fix

--- app/impero/src/Impero/Git/Controller/Git.php
+++ app/impero/src/Impero/Git/Controller/Git.php
@@ -5,9 +5,8 @@ use Pckg\Framework\Controller;
 class Git extends Controller
 {
 
-    public function getWebhookAction() {
+    public function postWebhookAction() {
         file_put_contents('/tmp/webhook.post.log', json_encode($_POST));
-        file_put_contents('/tmp/webhook.get.log', json_encode($_GET));
         dd('OK?');
         queue()->create('git:pull');
 
