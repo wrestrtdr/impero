bdc540e3e059433f64abb5bae2d3cc935839b51b
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-06-23 21:03:44 +0200

    Migrations

--- app/derive/config/migrations.php
+++ app/derive/config/migrations.php
@@ -3,6 +3,7 @@
 use Gnp\Orders\Migration\OrdersTags;
 use Pckg\Auth\Migration\CreateAuthTables;
 use Pckg\Dynamic\Migration\CreateDynamicTables;
+use Pckg\Dynamic\Migration\InstallDynamicTables;
 use Pckg\Generic\Migration\CreateGenericTables;
 
 return [
@@ -10,4 +11,5 @@ return [
     CreateDynamicTables::class,
     CreateGenericTables::class,
     OrdersTags::class,
+    //InstallDynamicTables::class,
 ];
