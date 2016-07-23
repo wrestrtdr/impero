53203b2a921bbb496725486f00945dd9843ec2bc
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-07-23 11:23:37 +0200

    FIx

--- app/derive/src/Derive/Orders/Controller/Orders.php
+++ app/derive/src/Derive/Orders/Controller/Orders.php
@@ -290,7 +290,7 @@ class Orders extends Controller
                                  'furs_zoi',
                                  'furs_confirmed_at',
                                  'furs_number' => function(Order $order) {
-                                     return $order->furs->id ?? null;
+                                     return $order->furs->furs_id ?? null;
                                  },
                              ]
                          );
