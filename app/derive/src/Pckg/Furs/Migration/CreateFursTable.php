5d24c02a68c0995662ca69ec105691b2b21abc58
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-07-23 10:51:01 +0200

    FIx

--- app/derive/src/Pckg/Furs/Migration/CreateFursTable.php
+++ app/derive/src/Pckg/Furs/Migration/CreateFursTable.php
@@ -7,8 +7,9 @@ class CreateFursTable extends Migration
 
     public function up()
     {
-        $furs = $this->table('furs', false, false);
+        $furs = $this->table('furs');
         $furs->integer('order_id');
+        $furs->varchar('furs_id');
         $furs->varchar('business_id');
         $furs->varchar('business_tax_number');
         $furs->datetime('requested_at')->nullable();
