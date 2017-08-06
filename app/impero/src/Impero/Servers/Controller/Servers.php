d8ffe6f54ea46d8b393a5039449efefa8b9ce7b7
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-08-06 23:45:40 +0200

    Path

--- app/impero/src/Impero/Servers/Controller/Servers.php
+++ app/impero/src/Impero/Servers/Controller/Servers.php
@@ -142,7 +142,7 @@ class Servers
         /**
          * We will generate ssh key for local www-data user to connect to server with impero username.
          */
-        $privateKey = path('app_private') . 'keys' . path('ds') . 'id_rsa_' . $server->id;
+        $privateKey = path('storage') . 'private' . path('ds') . 'keys' . path('ds') . 'id_rsa_' . $server->id;
         dd($privateKey);
         exec('ssh-keygen -b 4096 -t rsa -C \'' . $user . '@' . $ip . '\' -f ' . $privateKey . ' -N ""');
 
